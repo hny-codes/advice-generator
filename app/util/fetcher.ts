0648b8d7ff206bccd534aba0b47bb7617151a410
Henry <hnguyhenry@protonmail.com>
2024-02-19 16:04:20 -0800

    Add fetcher function for swr

---
+++ app/util/fetcher.ts
@@ -0,0 +1,9 @@
+// REFERENCE: https://github.com/vercel/swr/blob/main/examples/basic-typescript/libs/fetch.ts
+
+export default async function fetcher<JSON = any>(
+  input: RequestInfo,
+  init?: RequestInit
+): Promise<JSON> {
+  const res = await fetch(input, init);
+  return res.json();
+}
