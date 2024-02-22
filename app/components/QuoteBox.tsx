c5738298ee01239999f5a9bbe2b90a1d871a75b6
Henry <hnguyhenry@protonmail.com>
2024-02-21 21:26:34 -0800

    Add QuoteBox component

--- app/components/QuoteBox.tsx
+++ app/components/QuoteBox.tsx
@@ -1,46 +1,50 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { useQuote } from '../hook/useQuote';
 import RandomBtn from './RandomBtn';
-import { useSWRConfig } from 'swr';
+import getQuote from '../util/getQuote';
+
+type Slip = {
+  id: number;
+  advice: string;
+};
+
+type Quote = {
+  slip: Slip;
+};
 
 export default function QuoteBox() {
-  const { data, error, isLoading, mutate } = useQuote();
-  // const [quote, setQuote] = useState(data);
+  const [quote, setQuote] = useState<Quote>();
 
-  // const { mutate } = useSWRConfig();d
+  useEffect(() => {
+    async function initialQuote() {
+      setQuote(await getQuote());
+    }
+
+    initialQuote();
+  }, []);
 
   const handleQuote = async () => {
-    const newData = (await fetch('https://api.adviceslip.com/advice')).json();
-    mutate();
-    console.log(data?.slip.advice);
-    console.log(await newData);
-    
+    let info = await getQuote();
+    console.table(info);
+    setQuote(info);
   };
 
   return (
-    <div className='bg-[--clr-dark-gray-blue] min-w-full min-h-60 rounded-xl relative'>
+    <div className='bg-[--clr-dark-gray-blue] min-w-full min-h-52 rounded-xl relative'>
       <div className='text-center flex flex-col items-center gap-6 py-4 px-4'>
-        {isLoading ? (
-          <h1 className='text-[--clr-green] tracking-[0.2em] uppercase font-bold'>
-            Advice # ..
-          </h1>
-        ) : (
-          <h1 className='text-[--clr-green] tracking-[0.2em] uppercase font-bold'>
-            Advice # {data?.slip.id}
-          </h1>
-        )}
-        {isLoading ? (
-          <h2 className='text-[--clr-cyan] tracking-widest uppercase font-bold'>
-            Loading..
-          </h2>
-        ) : (
-          <h2 className='text-[--clr-cyan] font-bold text-2xl'>
-            &quot;{data?.slip.advice}&quot;
-          </h2>
-        )}
-        <svg className='mb-10' width='295' height='16' xmlns='http://www.w3.org/2000/svg'>
+        <h1 className='text-[--clr-green] tracking-[0.2em] uppercase font-bold'>
+          Advice # {quote?.slip.id}
+        </h1>
+        <h2 className='text-[--clr-cyan] font-bold text-2xl'>
+          &quot;{quote?.slip.advice}&quot;
+        </h2>
+        <svg
+          className='mb-8'
+          width='295'
+          height='16'
+          xmlns='http://www.w3.org/2000/svg'
+        >
           <g fill='none' fillRule='evenodd'>
             <path fill='#4F5D74' d='M0 8h122v1H0zM173 8h122v1H173z' />
             <g transform='translate(138)' fill='#CEE3E9'>
