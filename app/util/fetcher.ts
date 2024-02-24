748c0e9da521c5f351fd2b753566d70ef7072d28
Henry <hnguyhenry@protonmail.com>
2024-02-23 16:49:41 -0800

    Remove unneeded fetcher util

--- app/util/fetcher.ts
+++
@@ -1,9 +0,0 @@
-// REFERENCE: https://github.com/vercel/swr/blob/main/examples/basic-typescript/libs/fetch.ts
-
-export default async function fetcher<JSON = any>(
-  input: RequestInfo,
-  init?: RequestInit
-): Promise<JSON> {
-  const res = await fetch(input, init);
-  return res.json();
-}
