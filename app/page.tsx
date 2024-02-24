d05de029fe784bb542a720677c114605ef8f8e9f
Henry <hnguyhenry@protonmail.com>
2024-02-23 16:59:49 -0800

    Add width limit to page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import QuoteBox from './components/QuoteBox';
 export default function Home() {
   return (
     <main className='grid grid-cols-1 items-center justify-center h-screen'>
-      <div className='px-4'>
+      <div className='px-4 max-w-[--max-width] mx-auto'>
         <QuoteBox />
       </div>
     </main>
